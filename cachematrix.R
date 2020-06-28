8b0d6b2069a000f06b70b63c2b3dc539f63a0f38
barrosrubiralta <66692371+barrosrubiralta@users.noreply.github.com>
2020-06-28 14:08:04 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) { # 
+  m <- NULL #
+  set <- function(y) {
+    x <<- y #
+    m <<- NULL #
+  }
+  get <- function() x #
+  #setmean <- function(mean) m <<- mean #
+  setsolve<-function(solve) m <<- solve
+  #getmean <- function() m #
+  getsolve<-function() m
+  list(set = set,get = get,setsolve = setsolve,getsolve = getsolve)#Here is the other part of the "magic" in the operations of the makeVector() function. The last section of code assigns each of these functions as an element within a list(), and returns it to the parent environment.
+  
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  m <- x$getsolve() # 
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()#.
+  m <- solve(data)
+  x$setsolve(m)
+  m
 }
